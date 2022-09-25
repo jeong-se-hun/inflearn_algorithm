075e093957944ebb0a3566e4e266aac41e76ab9c
jeongsehun <tpgns933@gmail.com>
2022-09-25 11:42:31 +0900

    section3 init

---
+++ section3/q6.js
@@ -0,0 +1,12 @@
+// * 학급 회장(해쉬)
+
+// 투표용지에 쓰여져 있던 각 후보의 기호가 선생님이 발표한 순서대로 문자열로 입력됩니다.
+
+function solution(s) {}
+
+// * 입력예제
+let str = 'BACBACCACCBDEDE';
+
+//* 출력예제
+// C
+console.log(solution(str));
---
+++ section3/q7.js
@@ -0,0 +1,21 @@
+// * 아나그램(해쉬)
+
+// 첫 줄에 첫 번째 단어가 입력되고, 두 번째 줄에 두 번째 단어가 입력됩니다.
+// 단어의 길이는 100을 넘지 않습니다
+
+function solution(str1, str2) {}
+
+//* 입력예제 1
+let a = 'AbaAeCe';
+let b = 'baeeACA';
+
+//* 출력예제 1
+// YES
+
+//* 입력예제 2
+// let a = 'abaCC';
+// let b = 'Caaab';
+
+//* 출력예제 2
+// NO
+console.log(solution(a, b));
