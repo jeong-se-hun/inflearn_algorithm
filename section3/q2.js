feaa0a170b181b03a068d93884272594977982ef
jeongsehun <tpgns933@gmail.com>
2022-09-26 13:07:52 +0900

    09.26

--- section3/q2.js
+++ section3/q2.js
@@ -4,7 +4,27 @@
 // 두 번째 줄에 M개의 원소가 주어집니다. 원소가 중복되어 주어지지 않습니다.
 // 각 집합의 원소는 1,000,000,000이하의 자연수입니다.
 
-function solution(arr1, arr2) {}
+function solution1(arr1, arr2) {
+  const arr = [...arr1, ...arr2].sort((a, b) => a - b);
+  return arr.filter((n, i) => arr[i] === arr[i + 1]);
+}
+
+function solution2(arr1, arr2) {
+  arr1.sort((a, b) => a - b);
+  arr2.sort((a, b) => a - b);
+  let [p1, p2] = [0, 0];
+  let answer = [];
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] === arr2[p2]) {
+      answer.push(arr1[p1++]);
+      p2++;
+      continue;
+    }
+    arr1[p1] < arr2[p2] ? p1++ : p2++;
+  }
+  return answer;
+}
 
 //* 입력예제
 let a = [1, 3, 9, 5, 2];
@@ -12,4 +32,5 @@ let b = [3, 2, 5, 7, 8];
 
 //* 출력예제
 // 2 3 5
-console.log(solution(a, b));
+console.log(solution1(a, b));
+console.log(solution2(a, b));
--- section3/q3.js
+++ section3/q3.js
@@ -2,11 +2,47 @@
 
 // 수열의 원소값은 1,000을 넘지 않는 자연수이다.
 
-function solution(m, arr) {}
+function solution1(m, arr) {
+  let answer = 0;
+  let temp = 0;
+  let [p1, p2] = [0, 0];
+
+  while (p1 < arr.length && p2 < arr.length) {
+    temp += arr[p2];
+    if (temp === m) {
+      answer++;
+      p2 = p1++ + 1;
+      temp = 0;
+    } else if (temp > m) {
+      p2 = p1++ + 1;
+      temp = 0;
+    } else p2++;
+  }
+
+  return answer;
+}
+
+function solution2(m, arr) {
+  let answer = 0;
+  let temp = 0;
+  let p1 = 0;
+
+  for (let p2 = 0; p2 < arr.length; p2++) {
+    temp += arr[p2];
+    if (temp === m) answer++;
+    while (temp >= m) {
+      temp -= arr[p1++];
+      if (temp === m) answer++;
+    }
+  }
+
+  return answer;
+}
 
 //* 입력예제
 let a = [1, 2, 1, 3, 1, 1, 1, 2];
 
 //* 출력예제
 // 3
-console.log(solution(6, a));
+console.log(solution1(6, a));
+console.log(solution2(6, a));
