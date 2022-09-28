c7cdfcd603922991e8a1ebc4ceaef87909d4fa8c
jeongsehun <tpgns933@gmail.com>
2022-09-28 14:18:38 +0900

    09.28

--- section3/q4.js
+++ section3/q4.js
@@ -2,11 +2,40 @@
 
 // 수열의 원소값은 1,000을 넘지 않는 자연수이다.
 
-function solution(m, arr) {}
+function solution1(m, arr) {
+  let [answer, temp] = [0, 0];
+  let [p1, p2] = [0, 0];
+
+  while (p1 < arr.length) {
+    temp += arr[p2++];
+    if (temp <= m) answer++;
+    else {
+      p2 = ++p1;
+      temp = 0;
+    }
+  }
+
+  return answer;
+}
+function solution2(m, arr) {
+  let [answer, temp] = [0, 0];
+  let p1 = 0;
+
+  for (let p2 = 0; p2 < arr.length; p2++) {
+    temp += arr[p2];
+    while (temp > m) {
+      temp -= arr[p1++];
+    }
+    answer += p2 - p1 + 1;
+  }
+
+  return answer;
+}
 
 // * 입력예제
-let a = [1, 3, 1, 2, 3];
+let a = [1, 3, 1, 2, 3, 3, 1];
 
 //* 출력예제
 // 10
-console.log(solution(5, a));
+console.log(solution1(5, a));
+console.log(solution2(5, a));
--- section3/q5.js
+++ section3/q5.js
@@ -2,11 +2,37 @@
 
 // N개의 숫자열이 주어집니다. 각 숫자는 500이하의 음이 아닌 정수입니다.
 
-function solution(k, arr) {}
+// function solution(k, arr) {
+//   let answer = 0;
+//   let p2 = k;
+//   for (let p1 = 0; p1 < arr.length - (k - 1); p1++) {
+//     answer = Math.max(
+//       answer,
+//       arr.slice(p1, p2++).reduce((a, b) => a + b)
+//     );
+//   }
+//   return answer;
+// }
+
+function solution(k, arr) {
+  let answer = 0;
+  let sum = 0;
+
+  for (let i = 0; i < k; i++) sum += arr[i];
+  answer = sum;
+
+  for (let j = k; j < arr.length; j++) {
+    sum += arr[j] - arr[j - k];
+    answer = Math.max(answer, sum);
+  }
+
+  return answer;
+}
 
 //* 입력예제
 let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
 
 //* 출력예제
 // 56
+// console.log(solution(3, a));
 console.log(solution(3, a));
