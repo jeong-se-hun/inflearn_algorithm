393485a967be1d00f1a2f75dde9be75780e9c0f5
jeongsehun <tpgns933@gmail.com>
2022-10-05 12:33:46 +0900

    10.05

--- section4/q1.js
+++ section4/q1.js
@@ -1,6 +1,21 @@
 // * 올바른 괄호
 // 첫 번째 줄에 괄호 문자열이 입력됩니다. 문자열의 최대 길이는 30이다.
-function solution(s) {}
+
+function solution(s) {
+  let answer = 'YES';
+  let tempStack = [];
+
+  for (let x of s) {
+    if (x === '(') tempStack.push(x);
+    else if (x === ')') {
+      if (tempStack.length === 0) return 'NO';
+      tempStack.pop();
+    }
+  }
+
+  if (tempStack.length > 0) return 'NO';
+  return answer;
+}
 
 //* 입력예제
 let a = '(()(()))(()';
--- section4/q2.js
+++ section4/q2.js
@@ -3,15 +3,15 @@
 // 문자열이 주어진다. 문자열의 길이는 100을 넘지 않는다.
 
 function solution(s) {
-  let answer;
-  let stack = [];
+  let answer = [];
+
   for (let x of s) {
     if (x === ')') {
-      while (stack.pop() !== '(');
-    } else stack.push(x);
+      while (answer.pop() !== '(');
+    } else answer.push(x);
   }
-  answer = stack.join('');
-  return answer;
+
+  return answer.join('');
 }
 
 //* 입력예제
