872d86f1daf4cd0c1bcba06052fd11850e3d442d
jeongsehun <tpgns933@gmail.com>
2022-10-06 11:36:06 +0900

    10.06

--- section4/q3.js
+++ section4/q3.js
@@ -1,5 +1,24 @@
 // * 크레인 인형뽑기(카카오 기출)
-function solution(board, moves) {}
+function solution(board, moves) {
+  let answer = 0;
+  let stack = [];
+
+  moves.forEach(pos => {
+    for (let i = 0; i < board.length; i++) {
+      if (board[i][pos - 1] !== 0) {
+        let temp = board[i][pos - 1];
+        board[i][pos - 1] = 0;
+        if (temp === stack[stack.length - 1]) {
+          stack.pop();
+          answer += 2;
+        } else stack.push(temp);
+        break;
+      }
+    }
+  });
+
+  return answer;
+}
 
 let a = [
   [0, 0, 0, 0, 0],
--- section4/q4.js
+++ section4/q4.js
@@ -2,7 +2,22 @@
 
 // 첫 줄에 후위연산식이 주어집니다. 연산식의 길이는 50을 넘지 않습니다.
 // 식은 1~9의 숫자와 +, -, *, / 연산자로만 이루어진다
-function solution(s) {}
+function solution(s) {
+  let stack = [];
+
+  for (let x of s) {
+    if (!isNaN(x)) stack.push(Number(x));
+    else {
+      const [rt, lt] = [stack.pop(), stack.pop()];
+
+      if (x === '+') stack.push(lt + rt);
+      else if (x === '-') stack.push(lt - rt);
+      else if (x === '*') stack.push(lt * rt);
+      else if (x === '/') stack.push(lt / rt);
+    }
+  }
+  return stack[0];
+}
 
 // * 입력예제
 let str = '352+*9-';
